13f77f6aa4c2f5ed3a4b3c6f026ea43611a1546e
imtiazmahmudzidan <65275890+imtiazmahmudzidan@users.noreply.github.com>
2020-06-25 02:43:53 +0600

    Update sum_of_first_and_last_digit.c

--- sum_of_first_and_last_digit.c
+++ sum_of_first_and_last_digit.c
@@ -1,32 +1,25 @@
 /*A programme that takes input an intiger number and give sum of it's first and last digit */
 
 
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n, last, first, sum;
-    
-    
-    printf("enter the number\t");
-    
-    scanf("%d", &n);
-    
-    last = n % 10;
-    
-    while(n > 9){
-       n = n / 10;
-    }
-    
-    first = n;
-    
-    
-    sum = first + last;
-    
-    
-    printf("the sum is %d", sum);
-    
+    int T,N,i,MSD,LSD;
+    scanf("%d",&T);
+    for(i=1;i<=T;i++)
+    {
+        scanf("%d",&N);
+        LSD=N%10;
+       for(;N=N/10;)
+       {
+         if(N/10==0)
+         {
+             MSD=N%10;
+             break;
+         }
 
+       }
+       printf("Sum = %d\n",MSD+LSD);
+    }
     return 0;
-    
-    
 }
